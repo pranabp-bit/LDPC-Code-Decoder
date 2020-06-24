62edadc11ce7e6018c982985645b4121b48646c7
pranabp-bit <58358551+pranabp-bit@users.noreply.github.com>
2020-06-24 11:38:34 +0530

    Update RAM_SP_SR_RW_tb.v

--- memory/testbenches/RAM_SP_SR_RW_tb.v
+++ memory/testbenches/RAM_SP_SR_RW_tb.v
@@ -1,37 +1,69 @@
-module tb;
-  parameter ADDR_WIDTH = 8;
-  parameter DATA_WIDTH = 8;
-  parameter DEPTH = 256;
-  reg clk;
+// Testbench
+module test;
+   parameter DATA_WIDTH = 8;
+  parameter A_WIDTH = 8;
+  parameter RAM_DEPTH = 1<<A_WIDTH;
+
+  reg        clk;
+  reg  [DATA_WIDTH-1:0] data_write;
+  reg        write_enable;
   reg cs;
-  reg we;
-  reg [ADDR_WIDTH-1:0] address;
-  wire [DATA_WIDTH-1:0] data_out;
-  reg [DATA_WIDTH-1:0] tb_data;
-  RAM_SP_SR_RW #(DATA_WIDTH,ADDR_WIDTH,DEPTH)RAM(
-   .clk(clk),
-   .address(address),
-    .data_in(tb_data),
-   .cs(cs),
-   .we(we),
-   .data_out(data_out));
-  always #10 clk = ~clk;
+  reg  [A_WIDTH-1:0] address;
+  wire [DATA_WIDTH-1:0] data_read;
+  reg   [DATA_WIDTH-1:0] temp[(1<<A_WIDTH):0];
+  
+  RAM_SP_SR_RW #(DATA_WIDTH,A_WIDTH , RAM_DEPTH) RAM (
+    .clk(clk),
+    .address(address),
+    .data_in(data_write),
+    .we(write_enable),
+    .data_out(data_read),
+    .cs(cs));
+    
   initial begin
-    //dump waves
+    // Dump waves
     $dumpfile("dump.vcd");
-    $dumpvars(1, tb);
-    {clk, cs, we, address, tb_data} <= 0;
-    repeat (2) @ (posedge clk) begin
-    for (int i = 0; i < (1<<ADDR_WIDTH); i= i+1) begin
-      repeat (1) @(posedge clk) begin address <= i; we <= 1; cs <=1; tb_data <= $random %8;
+    $dumpvars(1, test);
+    cs=1;
+    clk = 1;
+    
+    write_enable = 0;
+    address = 0;
+    toggle_clk;
+    
+    
+
+    for(int i=0;i<(1<<A_WIDTH);i++)begin
+    //write a random value at each address of the ram
+    write_enable = 1;
+    address = i;
+    data_write = $random%(DATA_WIDTH) ;
+      temp[i]=data_write; //storing the random values in temp 
+    toggle_clk;
+    end
+    
+    
+    //read the earlier written values from each address of the ram
+    for(int i=0;i<(1<<A_WIDTH);i++)begin
+    address = i;
+    write_enable = 0;
+    toggle_clk;
+      //check whether the read values match with values written earlier
+      if(data_read!=temp[i])begin
+        $display("data from address %d was not read correctly",i);
+        #20 $finish;
       end
     end
-      for (int i = 0; i < (1<<ADDR_WIDTH); i= i+1) begin
-      repeat (1) @(posedge clk) begin
-        address <= i; we <= 0; cs <= 1;
-      end
-    end
-    end
+     
+    $display("All tests passed"); 
     #20 $finish;
   end
+  
+  task toggle_clk;
+    begin
+      #10 clk = ~clk;
+      #10 clk = ~clk;
+    end
+  endtask
+
 endmodule
