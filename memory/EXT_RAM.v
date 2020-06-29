904c07060464a6dbc1e64aeed69ed9d6102b6c0d
pranabp-bit <58358551+pranabp-bit@users.noreply.github.com>
2020-06-29 17:21:53 +0530

    Update EXT_RAM.v

--- memory/EXT_RAM.v
+++ memory/EXT_RAM.v
@@ -1,6 +1,6 @@
 `include "RAM_SP_SR_RW.v"
 module EXT_RAM (clk, address, data_in, we, cs, data_out); 
-  parameter DATA_WIDTH = 18;
+  parameter DATA_WIDTH = 15;
   parameter ADDR_WIDTH = 8;
   parameter RAM_DEPTH = 1<<ADDR_WIDTH;  
     // Write port
@@ -12,33 +12,33 @@ module EXT_RAM (clk, address, data_in, we, cs, data_out);
     // Read port
   output [DATA_WIDTH-1:0] data_out;
   reg    [DATA_WIDTH-1:0] data_out;
-  RAM_SP_SR_RW #( .DATA_WIDTH(6), 
+  RAM_SP_SR_RW #( .DATA_WIDTH(5), 
                   .ADDR_WIDTH(8), 
                   .RAM_DEPTH(256)) EXT_RAM_1
                 ( .clk(clk),
                   .address(address),
-                 .data_in(data_in[5:0]),
+                 .data_in(data_in[4:0]),
                   .we(cs),
                   .cs(cs),
-                 .data_out(data_out[5:0]));
+                 .data_out(data_out[4:0]));
   	                 
-  RAM_SP_SR_RW #( .DATA_WIDTH(6), 
+  RAM_SP_SR_RW #( .DATA_WIDTH(5), 
                   .ADDR_WIDTH(8), 
                   .RAM_DEPTH(256)) EXT_RAM_2
                 ( .clk(clk),
                   .address(address),
-                 .data_in(data_in[11:6]),
+                 .data_in(data_in[9:5]),
                   .we(cs),
                   .cs(cs),
-                 .data_out(data_out[11:5]));
+                 .data_out(data_out[9:5]));
 
-  RAM_SP_SR_RW #( .DATA_WIDTH(6), 
+  RAM_SP_SR_RW #( .DATA_WIDTH(5), 
                   .ADDR_WIDTH(8), 
                   .RAM_DEPTH(256)) EXT_RAM_3
                 ( .clk(clk),
                   .address(address),
-                 .data_in(data_in[17:12]),
+                 .data_in(data_in[14:10]),
                   .we(cs),
                   .cs(cs),
-                 .data_out(data_out[17:12]));  
+                 .data_out(data_out[14:10]));  
   endmodule
