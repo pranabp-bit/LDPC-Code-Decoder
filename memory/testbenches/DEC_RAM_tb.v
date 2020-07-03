acb594f6b077f0bef0ce2d62f8b85f81b3f2614f
pranabp-bit <58358551+pranabp-bit@users.noreply.github.com>
2020-07-03 19:33:47 +0530

    Update DEC_RAM_tb.v

--- memory/testbenches/DEC_RAM_tb.v
+++ memory/testbenches/DEC_RAM_tb.v
@@ -1,19 +1,20 @@
 // Testbench
 module test;
-   parameter DATA_WIDTH = 1;
-  parameter A_WIDTH = 8;
-  parameter RAM_DEPTH = 1<<A_WIDTH;
 
   reg        clk;
-  reg  [DATA_WIDTH-1:0] data_write;
+  reg  [7:0] address;
+  reg   data_write;
   reg        write_enable;
-  reg cs;
-  reg  [A_WIDTH-1:0] address;
-  wire [DATA_WIDTH-1:0] data_read;
-  reg   [DATA_WIDTH-1:0] temp[(1<<A_WIDTH):0];
-  reg rs;
-  
-  DEC_RAM #(DATA_WIDTH,A_WIDTH , RAM_DEPTH) RAM (
+  reg        cs;
+  wire  data_read;
+  reg        rs;
+  reg   temp[0:1][0:255];
+   
+  // Instantiate design under test
+  // DATA_WIDTH = 1
+  // ADDRESS_WIDTH = 8
+  // RAM_DEPTH = 2^A_WIDTH = 256
+  DEC_RAM #(1, 8, 256) RAM (
     .clk(clk),
     .address(address),
     .data_in(data_write),
@@ -21,77 +22,87 @@ module test;
     .data_out(data_read),
     .cs(cs),
     .rs(rs));
-  
     
   initial begin
     // Dump waves
     $dumpfile("dump.vcd");
     $dumpvars(1, test);
     cs=1;
+    rs=0;
     clk = 1;
-    
     write_enable = 0;
-    address = 0;
-    toggle_clk;
-    
-    
 
-    for(int i=0;i<(1<<A_WIDTH);i++)begin
-      rs=1'b0;
-    //write a random value at each address of the ram
+    $display("Read initial data.");
+    toggle_clk;
+    $display("data[%0h]: %0h",
+      address, data_read);
+    
+    for(int i=0;i<256;i++)begin 
+    $display("Write new data.");
+    rs=0;
     write_enable = 1;
     address = i;
-      data_write = $random%(DATA_WIDTH+1) ;
-      temp[i]=data_write; //storing the random values in temp 
+    data_write = $random%2;
+      temp[0][i]=data_write;
     toggle_clk;
     end
     
+        
+    #10;
+    for(int i=0;i<256;i++)begin
+    $display("Write new data.");
+    rs=1;
+    write_enable = 1;
+    address = i;
+      data_write = $random %2;
+      temp[1][i]=data_write;
+    toggle_clk;
+    end
     
-    //read the earlier written values from each address of the ram
-    for(int i=0;i<(1<<A_WIDTH);i++)begin
-      rs=1'b0;
+    #10;
+    for(int i=0;i<256;i++)begin 
+    rs=0;
+    $display("Read new data.");
     address = i;
     write_enable = 0;
     toggle_clk;
-      //check whether the read values match with values written earlier
-      if(data_read!=temp[i])begin
-        $display("data from address %d was not read correctly",i);
-        #20 $finish;
+    $display("data[%0h]: %0h",
+      address, data_read);
+      if(data_read!=temp[0][i])begin
+        $display("read incorrectly on address %0h",address);
+        $finish;
       end
     end
-        for(int i=0;i<(1<<A_WIDTH);i++)begin
-      rs=1'b1;
-    //write a random value at each address of the ram
-    write_enable = 1;
-    address = i;
-      data_write = $random%(DATA_WIDTH+1) ;
-      temp[i]=data_write; //storing the random values in temp 
-    toggle_clk;
-    end
     
     
-    //read the earlier written values from each address of the ram
-    for(int i=0;i<(1<<A_WIDTH);i++)begin
-      rs=1'b1;
+    #10;
+    for(int i=0;i<256;i++)begin
+    rs=1;
+    $display("Read new data.");
     address = i;
     write_enable = 0;
     toggle_clk;
-      //check whether the read values match with values written earlier
-      if(data_read!=temp[i])begin
-        $display("data from address %d was not read correctly",i);
-        #20 $finish;
-      end
-    end
-     
-    $display("All tests passed"); 
-    #20 $finish;
+      if(data_read!=temp[1][i])begin
+        $display("read incorrectly on address %0h",address);
+        $finish;
+      end      
+    $display("data[%0h]: %0h",
+      address, data_read);
+    end  
+    $display("all tests passed");
+
+
   end
   
   task toggle_clk;
     begin
       #10 clk = ~clk;
       #10 clk = ~clk;
+      #10 clk = ~clk;
+      #10 clk = ~clk;       
     end
   endtask
+
+
 
 endmodule
