8bc5ebc430c892ead725a824d4410b6dae1905ca
pranabp-bit <58358551+pranabp-bit@users.noreply.github.com>
2020-07-06 00:21:57 +0530

    Update AG.v

--- address_generator/AG.v
+++ address_generator/AG.v
@@ -1,1 +1,22 @@
+`include "UP_COUNTER.v"
+module ADDRESS_GENERATOR(clk,reset,en,out);
+      parameter DATA_WIDTH   = 8;
+      parameter COUNT_FROM   = 0;
+      
 
+      input clk;   // clock input
+      input en;  // enable
+      input reset; // reset
+
+
+      output reg [DATA_WIDTH-1:0] out;
+
+  UP_COUNTER #(DATA_WIDTH, COUNT_FROM) AG 
+    (
+    .clk(clk),
+    .rstn(reset),
+    .en(en),
+    .out(out)
+    );
+
+endmodule
