596d6461222d9669527a5e4afa7ca36403aebdce
pranabp-bit <58358551+pranabp-bit@users.noreply.github.com>
2020-07-05 19:43:10 +0530

    Update UP_COUNTER.v

--- address_generator/UP_COUNTER.v
+++ address_generator/UP_COUNTER.v
@@ -1,1 +1,21 @@
-
+module up_counter(clk, rstn, out);
+  
+    parameter DATA_WIDTH = 8;
+    parameter COUNT_FROM=0;
+  
+    input clk;               
+    input rstn;             
+    output reg[DATA_WIDTH-1:0] out;   
+  
+  
+  
+  
+  always @ (posedge clk) begin
+    if (rstn) begin
+      out <= COUNT_FROM;
+    end 
+    else begin 
+      out <= out + 1;
+    end
+  end
+endmodule
